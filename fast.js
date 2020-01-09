05907b76ddb2027ec1e1d600435fdbec639a29e8
mangochanghee <oko0412o@naver.com>
2020-01-09 16:03:26 +0900

    prototype

--- fast.js
+++ fast.js
@@ -1,38 +1,25 @@
-// 객체 object
-// 함수, 클래스(틀) =? 객체, 개체, object
+// 프로토타입 확장
 
-// function 틀() {} => new 틀()
+function Person() {}
 
-function A() {}
-
-const a = new A();
-
-console.log(a,typeof a)
-console.log(A());
-
-
-function B(name,age) {
-    console.log(name,age)
-}
-const b = new B();
-const c = new B('Mark', 22);
-
-console.log(B());
-
-
-// 객체 속성 추가하기 property
-// {사용할 데이터, 함수}
-function D(name){
-    this.name = 'Mark'
+Person.prototype.hello = function(){
+    console.log('hello');
 }
 
-const d = new D();
-console.log(d)
-
-function F(){
-    this.hello = function(){
-        console.log('hello')
-    };
+function Korean(region){
+    this.region = region;
+    this.where = function(){
+        console.log('where', this.region);
+    }
 }
 
-new F().hello();
+Korean.prototype = Person.prototype
+
+const k = new Korean('seoul')
+
+k.hello();
+k.where();
+
+console.log(k instanceof Korean);
+console.log(k instanceof Person);
+console.log(k instanceof Object);
