5fe3765d9e5c403529e53ab9e487f764a4410198
mangochanghee <oko0412o@naver.com>
2020-01-22 17:56:58 +0900

    add 추가 완료

--- JS/project/js/add.js
+++ JS/project/js/add.js
@@ -16,10 +16,48 @@ async function getUserByToken(token){
     }
 }
 
-function save(){
+async function save(){
     event.preventDefault();
     event.stopPropagation();
     console.log('save')
+    event.target.classList.add('was-validated'); // bootstrap 기능 버튼에 문제가 있음을 표시
+
+    const titleElement = document.querySelector('#title')
+    const messageElement = document.querySelector('#message')
+    const authorElement = document.querySelector('#author')
+    const urlElement = document.querySelector('#url')
+
+
+    const title = titleElement.value;
+    const message = messageElement.value;
+    const author = authorElement.value;
+    const url = urlElement.value;
+
+    if (title === '' || message ==='' || author ===''||url ===''){
+        alert('필요항목이 부족합니다.')
+        return;
+    }
+    const token = getToken();
+    if (token ===null){
+        location.assign('/login');
+        return;
+    }
+    try{
+        await axios.post('https://api.marktube.tv/v1/book',{
+            title,
+            message,
+            author,
+            url,
+        },{
+        headers:{
+            Authorization:`Bearer ${token}`
+        }
+    });
+    location.assign('/');
+    } catch(error){
+        // console.log('save error', error);
+        alert('책 추가 실패');
+    }
 }
 
 function bindSaveButton(){
