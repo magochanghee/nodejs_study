6394a9fa8cb0bc788352d70de355b011640d10b5
mangochanghee <oko0412o@naver.com>
2020-01-29 15:41:26 +0900

    book delete error clear

--- JS/project/js/book.js
+++ JS/project/js/book.js
@@ -1,27 +1,3 @@
-async function logout() {
-    const token = getToken();
-    if (token === null) {
-      location.assign('/login');
-      return;
-    }
-    try {
-      await axios.delete('https://api.marktube.tv/v1/me', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-    } catch (error) {
-      console.log('logout error', error);
-    } finally {
-      localStorage.clear();
-      location.assign('/login');
-    }
-  }
-
-function bindLogoutButton(){
-    const btnLogout =document.querySelector('#btn_logout');
-    btnLogout.addEventListener('click',logout);
-}
 function getToken() {
     return localStorage.getItem('token');
 }
@@ -39,10 +15,31 @@ async function getUserByToken(token){
     }
 }
 
+async function logout() {
+    const token = getToken();
+    if (token === null) {
+      location.assign('/login');
+      return;
+    }
+    try {
+      await axios.delete('https://api.marktube.tv/v1/me', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } catch (error) {
+      console.log('logout error', error);
+    } finally {
+      localStorage.clear();
+      
+      window.location.location= '/login';
+    }
+}
+
 async function getBook(bookId){
     const token = getToken();
     if(token ===null){
-        location.assign('/login');
+        location.href= '/login';
         return null;
     }
     try{
@@ -64,12 +61,18 @@ async function deleteBook(bookId){
         location = '/login';
         return;    
     }
-    await axios.delete(`https://api.marktube.tv/v1/book/${bookId}`,{
-        headers:{
-            Authorization:`Bearer: ${token}`,
-        }
-    });
+    await axios.delete(`https://api.marktube.tv/v1/book/${bookId}`, {
+        headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 }
+
+function bindLogoutButton(){
+    const btnLogout =document.querySelector('#btn_logout');
+    btnLogout.addEventListener('click',logout);
+}
+
 
 function render(book){
     const detailElement = document.querySelector('#detail');
@@ -89,10 +92,11 @@ function render(book){
         </div>
     </div>`;
 
-    document.querySelector('#btn-delete').addEventListener('click', async ()=>{
+    document.querySelector('#btn-delete').addEventListener('click', async () => {
         try{
+            console.log(book);
             await deleteBook(book.bookId);
-            location.assign('/');
+            location.href='/';
         }catch (error){
             console.log('error',error);
         }
@@ -103,19 +107,19 @@ async function main(){
     //버튼에 이벤트 연결
     bindLogoutButton();
     //브라우저에서 id 가져오기
-    const bookId = new URL(location.href).searchParams.get('id')
+    const bookId = new URL(location.href).searchParams.get('id');
 
     //토큰 체크
     const token = getToken();
     if (token === null){
-        location.assign('/login');
+        location.href= '/login';
         return;
     }
     //토큰으로 서버에서 내정보 가져오기
     const user = await getUserByToken(token);
     if (user === null){
         localStorage.clear();
-        location.assign('/login');
+        location.href='/login';
         return;
     }
     //책을 서버에서 받아오기
---
+++ test.py
@@ -0,0 +1,18 @@
+{
+    "text": "Danny Torrence left a 1 star review for your property.",
+    "blocks": [  
+        {
+            "type": "section",
+            "block_id": "section567",
+            "text": {
+                "type": "mrkdwn",
+                "text": "오늘도 왔습니다 척추요정!!\n\n다들 구부정한 자세로 모니터를 보고 있지 않나요??\n\n어서어서!!\n\n허리를 쭉 펴고 기지개를 한번 시원~~~~~~하게 하세요~!!!\n\n여러분의 척추는 소중하니깐요(찡끗)\n"
+            },
+            "accessory": {
+                "type": "image",
+                "image_url": "https://4.bp.blogspot.com/-O2caITWpAYI/WnXN1Jt7n5I/AAAAAAAAETc/TQUomRKVesApkkCHjzEl_XsD16fGXmNvQCLcBGAs/s1600/ezgif.com-video-to-gif%2B%252823%2529.gif",
+                "alt_text": "안하면 수술각"
+            }
+        },
+    ]
+}
